1c3758e51bd82456ebd36efbadcaf5674f4a6eb4
kjayru <wiltinoco@gmail.com>
2014-08-26 18:49:50 -0500

    notas
    asd

--- inc.footer.php
+++ inc.footer.php
@@ -23,11 +23,6 @@
             <li><a href="javascript:share('renren')" rel="nofollow"><i class="icon_bg icon_renren"></i>Renren</a></li>
 
     </ul>
-
-
-
-    
-
     <div class="loadingContentWrapper">
 
         <div class="loadingContent">
@@ -38,6 +33,169 @@
 
     </div>
 
+<div class="modal hide" id="mediaDetail" style="top: 34px; display: none;" aria-hidden="false">
+        <div class="modal-body">
+            <div class="mediaDetailContainer"><div class="mediaDetail" >
+        <button type="button" class="close" data-dismiss="modal">×</button>
+        <div class="bigPhoto" style="z-index: 8006;">
+            <img src="http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10601854_733935986666973_1337058991_a.jpg" style="opacity: 0.8;filter: alpha(opacity=80);-webkit-filter: blur(2px); filter: blur(2px);">
+        </div>
+        
+        <div class="pic-load">
+            <div class="bubblingG">
+                <span id="bubblingG_1"></span>
+                <span id="bubblingG_2"></span>
+                <span id="bubblingG_3"></span>
+            </div>
+            
+        </div>
+        
+        <div class="bigPhoto" style="z-index: 8008; background: none;">
+            <img src="http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10601854_733935986666973_1337058991_n.jpg">
+            
+            <div class="prevNextWrapper" style="display: block;">
+                <div class="prevButton prevNext" style="left: 14px; display: none;">
+                    <img class="thumb" src="">
+                    <img class="big" src="">
+                    <a href="javascript:void(0);" title="Previous (Left Key)" rel="nofollow">
+                        <img class="arrow" onmousedown="switchDetailPrev()" src="/Content/MultiView/prevButton.png" alt="Previous (Left Key)"></a>
+                </div>
+                <div class="nextButton prevNext" style="right: 14px;">
+                    <img class="thumb" src="http://scontent-b.cdninstagram.com/hphotos-xpa1/t51.2885-15/1515938_677404502312628_218265632_a.jpg">
+                    <img class="big" src="http://scontent-b.cdninstagram.com/hphotos-xpa1/t51.2885-15/1515938_677404502312628_218265632_n.jpg">
+                    <a href="javascript:void(0);" title="Next (Right Key)" rel="nofollow">
+                        <img class="arrow" onmousedown="switchDetailNext()" src="/Content/MultiView/nextButton.png" alt="Next (Right Key)"></a>
+                </div>
+            </div>
+            <div class="photoExtraInfo selectable" style="z-index: 8009; display: block;">
+                <div>
+                    
+                    Filter: <span class="selectable">Normal</span> | 
+                    
+                    <span class="selectable">2014-8-26 16:01:03</span> | <a href="http://pinsta.me/p/795800751114797203_20388055" target="_blank"><span class="selectable detailPermalink">Permalink</span></a>
+
+                    
+
+                    | <a href="javascript:showQrCode('scanQrCodeForInstagramHook','http://instagram.com/p/sLQJEaL8CT/')" class="hoverZoomLink">Open in Instagram App</a>
+
+                </div>
+            </div>
+            
+        </div>
+        <div class="detailSidebar">
+            <div class="userInfoBar clearfix">
+
+                    <a class="userAvatarLink" href="/love_food" title="love_food" target="_blank">
+                        <img src="http://images.ak.instagram.com/profiles/profile_20388055_75sq_1381106259.jpg"></a>
+                    <div class="userRightInfo">
+                        <a class="userAvatarLink" href="/love_food" target="_blank">love_food </a>
+                    </div>
+
+            </div>
+            
+            <div class="detailCaption emojstext">Tag someone who would<span class="emoji emoji2764" title="heavy black heart"></span>️this</div>
+            
+            <div class="detailStats">
+                <div class="btn-group">
+                    <a class="btn btn-danger likeDropDownButton likeButton" href="javascript:like();" rel="nofollow" data-original-title=""><i class="icon-heart icon-white"></i><span class="count">36925</span>
+                    </a>
+                    <a class="btn dropdown-toggle likeDropDownButtonCaret" data-toggle="dropdown">
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        
+                        <li><a href="/valenirigoyen1" target="_blank"><i class="icon-heart"></i>valenirigoyen1</a></li>
+                        
+                        <li><a href="/dillonbeehlerrr" target="_blank"><i class="icon-heart"></i>dillonbeehlerrr</a></li>
+                        
+                        <li><a href="/shelbybutler1418" target="_blank"><i class="icon-heart"></i>shelbybutler1418</a></li>
+                        
+                        <li><a href="/h.ei.di" target="_blank"><i class="icon-heart"></i>h.ei.di</a></li>
+                        
+                    </ul>
+                </div>
+                
+<div class="btn-group tasksButton">
+    <a class="btn btn-danger dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-tasks icon-white"></i>
+        <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+        <li><a href="javascript:deleteMedia()"><i class="icon-remove"></i>Delete</a></li>
+    </ul>
+</div>
+
+                <a href="javascript:void(0)" class="sendButton btn btn-success" rel="nofollow"><i class="icon-comment icon-white"></i>Comment</a>
+                <span style="float: right">
+                    <a onmousedown="showBrickShareMenu(this)" class="btn btn-primary dropdown-toggle" href="javascript:void(0)" title="Share" rel="nofollow"><i class="icon-share-alt icon-white"></i></a>
+                </span>
+            </div>
+            <div class="addCommentContainer">
+                <textarea class="addComment" cols="20" id="addComment" name="addComment" placeholder="Comment" rows="2"></textarea>
+                <div class="loadingComments">
+                    <img src="/Content/Images/ajax-loader-arrow.gif" alt="">
+                </div>
+                <a href="javascript:void(0);" class="showAllComments" rel="nofollow"><i class="icon-chevron-down"></i>View all 696 comments</a>
+            </div>
+            <div class="detailComments jspScrollable" style="height: 381px; overflow: hidden; padding: 0px; width: 302px;" tabindex="0">
+                
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+            <div class="jspContainer" style="width: 302px; height: 381px;">
+            <div class="jspPane" style="padding: 0px; width: 288px; top: -129px;">
+            
+            <div class="comment clearfix" id="detailComment_795841249753940553" fromid="1410955021" commentid="795841249753940553">
+        <div class="commentProfileImage">
+            <a href="/theejodii" title="Thee Jodii" target="_blank">
+                <img src="http://photos-a.ak.instagram.com/hphotos-ak-xfp1/10471830_232082643667800_41029900_a.jpg"></a>
+        </div>
+        <div class="commentBody">
+            <a class="commentUserName" href="/theejodii" target="_blank">theejodii</a>
+            <a href="javascript:void(0);" class="commentTinyButton mentionUser btn btn-info btn-mini" rel="nofollow">@mention</a>
+            <a href="javascript:void(0);" class="commentTinyButton deleteComment btn btn-danger btn-mini" rel="nofollow">Delete</a><span class="commentCreateTime">3 minuntes ago </span>
+            <br>
+            <span class="detailCommentText emojstext">Yummy</span>
+        </div>
+    </div>
+    
+   
+    
+    </div>
+    <div class="jspVerticalBar">
+    <div class="jspCap jspCapTop"></div>
+    <div class="jspTrack" style="height: 381px;">
+    <div class="jspDrag" style="height: 285px; top: 96px;">
+    <div class="jspDragTop"></div>
+    <div class="jspDragBottom"></div>
+    </div></div>
+    <div class="jspCap jspCapBottom"></div>
+    </div></div></div>
+            
+            
+                <div class="detailLocationName" style="color: #D1C286; text-align: right;">
+                    
+                    
+                
+                </div>                
+
+            
+        </div>
+    </div></div>
+        </div>
+    </div>
     <div class="modal hide" id="mediaDetail">
 
         <div class="modal-body">
